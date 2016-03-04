c92c48e55bdcd0d07db30201928d2b4a9179e38b
Your Name <you@example.com>
2016-03-03 21:11:25 -0300

    All api is working in Play example. I will add some tests

--- play_example/app/controllers/TasksController.scala
+++ play_example/app/controllers/TasksController.scala
@@ -5,11 +5,14 @@ import javax.inject._
 import akka.util.Timeout
 import com.github.frossi85.database.DB
 import com.github.frossi85.domain.Task
-import com.github.frossi85.services.{TaskActor, TaskService}
+import com.github.frossi85.services.{TaskRequest, TaskActor, TaskService}
 import play.api.libs.json._
 import play.api.mvc._
 import slick.jdbc.JdbcBackend
+import play.api.data._
+import play.api.data.Forms._
 
+import scala.concurrent.Future
 
 @Singleton
 class TasksController @Inject() (system: ActorSystem) extends Controller {
@@ -31,29 +34,54 @@ class TasksController @Inject() (system: ActorSystem) extends Controller {
   implicit val taskImplicitWrites = Json.writes[Task]
   implicit val taskImplicitReads = Json.reads[Task]
 
+  val taskForm = Form(
+    mapping(
+      "name" -> text,
+      "description" -> text
+    )(TaskRequest.apply)(TaskRequest.unapply)
+  )
 
-
-  def tasks = Action.async {
-
-
-
-    (service ? TaskActor.GetTasksByUserId(userId)).mapTo[Seq[Task]].map(x => Ok(Json.toJson(x)))
-    //taskService.byUser(userId).map(x => Ok(Json.toJson(x)))
+  def list = Action.async {
+    (service ? TaskActor.GetTasksByUserId(userId))
+      .mapTo[Seq[Task]]
+      .map(x => Ok(Json.toJson(x)))
   }
 
-  /*
-
-  def healthCheck = Action {
-    Ok("It's Alive")
+  def byId(id: Long) = Action {
+    Ok(s"byId $id")
   }
 
-  def createSchemas = Action {
-    DB.createSchemas()
-    Ok("Schemas created")
+  def create = Action.async { implicit request =>
+    taskForm.bindFromRequest.fold(
+      formWithErrors => {
+        // binding failure, you retrieve the form containing errors:
+        Future(BadRequest("Something went wrong!!!"))
+      },
+      taskRequest => {
+        (service ? TaskActor.CreateTaskFromRequest(userId, taskRequest))
+          .mapTo[Task]
+          .map(x => Ok(Json.toJson(x)))
+      }
+    )
   }
 
-  def populateDatabase = Action {
-    DB.populateWithDummyData()
-    Ok("Database populated with dummy data")
-  }*/
+  def update(id: Long) = Action.async { implicit request =>
+    taskForm.bindFromRequest.fold(
+      formWithErrors => {
+        // binding failure, you retrieve the form containing errors:
+        Future(BadRequest("Something went wrong!!!"))
+      },
+      taskRequest => {
+        (service ? TaskActor.UpdateTaskFromRequest(id, taskRequest)).mapTo[Option[Task]].map(t => t match {
+          case Some(task) => Ok(Json.toJson(task))
+          case None => NotFound("There is no task with this id")
+        })
+      }
+    )
+  }
+
+  def delete(id: Long) = Action.async { implicit request =>
+      (service ? TaskActor.DeleteTaskById(id))
+        .map(x => Ok(s"Task with id=$id was deleted"))
+  }
 }
