18aa935ed1a970ab22c4e67f93d3e612e4fc3d39
lregnier <leonardoregnier@gmail.com>
2016-08-12 11:18:03 -0300

    lregnier: small fixes.

--- load-test/src/test/scala/com/whiteprompt/test/TaskTests.scala
+++ load-test/src/test/scala/com/whiteprompt/test/TaskTests.scala
@@ -1,34 +1,27 @@
-package com.whiteprompt.test
-
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
+import scala.concurrent.duration._
 import scala.util.Random
 
 object TaskTests {
   private def randomString = Random.alphanumeric.take(10).mkString
-  private def newTask =  s"""{
+  private def taskData =  s"""{
     "name": "$randomString",
     "description": "$randomString"
   }"""
 
-  private def updatedTask = "{" +
-    "\"id\": \"${taskId}\", " + s"""
-    "name": "$randomString",
-    "description": "$randomString"
-  }"""
-
-	val createTask = exec(
-    http("CreateTask") // let's give proper names, as they are displayed in the reports
-		  .post("/tasks")
+  val createTask = exec(
+    http("CreateTask")
+      .post("/tasks")
       .header("Content-Type", "application/json")
-      .body(StringBody(newTask))
+      .body(StringBody(taskData))
       .asJSON
       .check(jsonPath("$.id").saveAs("taskId"))
       .check(status is 201)
   )
 
-  val viewTask = exec(
-    http("ViewTask")
+  val retrieveTask = exec(
+    http("RetrieveTask")
       .get("/tasks/${taskId}")
       .header("Content-Type", "application/json")
       .asJSON
@@ -36,29 +29,52 @@ object TaskTests {
       .check(status is 200)
   )
 
-  val listTasks = exec(
-    http("ListTask")
-      .get("/tasks")
-      .header("Content-Type", "application/json")
-      .asJSON
-      .check(status is 200)
-  )
-
   val updateTask = exec(
-    http("ViewTask")
+    http("UpdateTask")
       .put("/tasks/${taskId}")
       .header("Content-Type", "application/json")
-      .body(StringBody(updatedTask))
+      .body(StringBody(taskData))
       .asJSON
       .check(jsonPath("$.id"))
       .check(status is 200)
   )
 
   val deleteTask = exec(
-    http("ViewTask")
+    http("DeleteTask")
       .delete("/tasks/${taskId}")
       .header("Content-Type", "application/json")
       .asJSON
       .check(status is 200)
   )
+
+  val listTasks = exec(
+    http("ListTasks")
+      .get("/tasks")
+      .header("Content-Type", "application/json")
+      .asJSON
+      .check(status is 200)
+  )
+}
+
+class TaskTests extends Simulation {
+  import TaskTests._
+
+  val host = "localhost"
+  val port = 9000
+
+  val scenarioName = "CreateUpdateListViewTasks"
+
+  val httpProtocol = http
+    .baseURL(s"http://$host:$port/v1")
+
+  val scn = scenario(scenarioName)
+    .exec(
+      createTask,
+      retrieveTask,
+      updateTask,
+      listTasks,
+      deleteTask
+    )
+
+  setUp(scn.inject(rampUsers(10) over (10 seconds))).protocols(httpProtocol)
 }
